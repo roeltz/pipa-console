832769774992277385fbb5650124e92ce4bfd4f2
Leonardo Rothe <leonardo.rothe@shiftactive.com>
2018-06-08 14:13:24 -0500

    Put a lacking use statement in ConsoleContext

--- src/Pipa/Console/ConsoleContext.php
+++ src/Pipa/Console/ConsoleContext.php
@@ -4,22 +4,23 @@ namespace Pipa\Console;
 use Pipa\Dispatch\Context;
 use Pipa\Dispatch\Dispatch;
 use Pipa\Dispatch\Response;
+use Pipa\Dispatch\Router;
 use Pipa\Error\ErrorHandler;
 
 class ConsoleContext extends Context {
-	
+
 	const CONTEXT_ID = 'console';
-	
+
 	function dispatch(Router $router) {
 		ErrorHandler::setContext(self::CONTEXT_ID);
-		
+
 		$dispatch = new Dispatch(
 			$this,
 			$router,
 			new Request,
 			new Response
 		);
-		
+
 		return $dispatch->run();
 	}
 }
